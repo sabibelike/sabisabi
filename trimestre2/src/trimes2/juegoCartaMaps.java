425a5632efada2772caee97e08dc27754615bea6
Andrea Dal <andreasabina03@gmail.com>
2024-01-16 22:09:46 +0100

    actualizacion juego cartas

--- trimestre2/src/trimes2/juegoCartaMaps.java
+++ trimestre2/src/trimes2/juegoCartaMaps.java
@@ -4,7 +4,7 @@ public class juegoCartaMaps {
 
 	public static void main(String[] args) {
 		
-
+		//tienes que hacer este juego porque es bastante bueno para que practiques malparia <3
 	}
 	public static void mazoCartas() {
 	String[][] bastillo = new String ["bastillo"]["1 "];
