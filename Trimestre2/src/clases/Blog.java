7695346d60bcccb63295bab00f9fbf2b77f1d35f
AndreaSabinaDal-Alum <andreasabina03@gmail.com>
2024-01-17 13:12:44 +0100

    x2

--- Trimestre2/src/clases/Blog.java
+++ Trimestre2/src/clases/Blog.java
@@ -4,45 +4,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Blog {
-	//atributos
+	// atributos
 	private String nombreBlog;
 	private List<Publicacion> listaPublicaciones;
-	
-	//constructor
+
+	// constructor
 	public Blog(String nombreBlog) {
 		this.nombreBlog = nombreBlog;
 		this.listaPublicaciones = new ArrayList<Publicacion>();
 	}
-	
-	//metodos
+
+	// metodos
 	/*
-	 * agregarpublicacion*/
-	public void agregarPublicacion(Publicacion publicacion) {
-		
-	}
-	
-	
-	 /* agregarcomentario
-	 * buscarpublicacionesporautor
-	 * obtenerComentariosDePublicacion 
-	 * 
+	 * agregarpublicacion
 	 */
-	
-	
-	//getters and setters
+	public void agregarPublicacion(Publicacion publicacion) {
+		listaPublicaciones.add(publicacion);
+	}
+
+	/* agregarcomentario */
+	public void agregarComentario(Publicacion publicacion, Comentario comentario) {
+		if (listaPublicaciones.contains(publicacion)) {
+			publicacion.agregarComentario(comentario);
+		} else {
+			System.out.println("La publicacion no existe en el blog");
+		}
+	}
+
+	/* buscarpublicacionesporautor */
+	public void buscarPubliPorAutor(Publicacion publicacion, String autor) {
+		System.out.println("Las publicaciones de este autor son: ");
+		for (Publicacion autores : listaPublicaciones) {
+			if (publicacion.getAutor().equals(autor)) {
+				System.out.println(publicacion.getContenidoPubli());
+			} else {
+				System.out.println(autor + " no tiene ninguna publicación");
+			}
+		}
+	}
+
+	/*
+	 * obtenerComentariosDePublicacion*/
+	 public void obtenerComentarioPubli(Publicacion publicacion) {
+		 publicacion.getComentarios();
+	 }
+
+	// getters and setters
 	public String getNombreBlog() {
 		return nombreBlog;
 	}
+
 	public void setNombreBlog(String nombreBlog) {
 		this.nombreBlog = nombreBlog;
 	}
+
 	public List<Publicacion> getListaPublicaciones() {
 		return listaPublicaciones;
 	}
+
 	public void setListaPublicaciones(List<Publicacion> listaPublicaciones) {
 		this.listaPublicaciones = listaPublicaciones;
 	}
-	
-	
 
 }
